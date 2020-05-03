d3d0c79171697bbf456dbf86fbbc116425bc3344
Veller <vellerbauer@gmail.com>
2020-05-03 18:58:28 -0300

    added post and get methods

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,17 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+interface TransactionsAndBalance {
+  transactions: Transaction[];
+  balance: Balance;
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -13,16 +24,39 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
+  public transactionsAndBalance(): TransactionsAndBalance {
+    return {
+      transactions: this.all(),
+      balance: this.getBalance(),
+    };
+  }
+
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const totalIncome = this.transactions
+      .filter(transaction => transaction.type === 'income')
+      .reduce((acc, currentValue) => acc + currentValue.value, 0);
+
+    const totalOutcome = this.transactions
+      .filter(transaction => transaction.type === 'outcome')
+      .reduce((acc, currentValue) => acc + currentValue.value, 0);
+
+    const total = totalIncome - totalOutcome;
+
+    return {
+      income: totalIncome,
+      outcome: totalOutcome,
+      total,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
